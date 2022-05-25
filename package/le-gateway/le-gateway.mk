da815f9fb51951c818a3f2b6979dff2a41a2f052
Kim Tae-Hee <thkim7816@gmail.com>
2022-05-25 19:31:52 +0900

    수정: le-gateway

--- package/le-gateway/le-gateway.mk
+++ package/le-gateway/le-gateway.mk
@@ -8,7 +8,7 @@ LE_GATEWAY_SITE:=$(TOPDIR)/../pkg_src/le-gateway
 LE_GATEWAY_SITE_METHOD:=local
 LE_GATEWAY_INSTALL_STAGING:=NO
 LE_GATEWAY_INSTALL_TARGET:=YES
-LE_GATEWAY_DEPENDENCIES := plog
+LE_GATEWAY_DEPENDENCIES := plog nlohmannjson sdbuscpp
 
 
 $(eval $(cmake-package))
--- pkg_src/le-gateway/main.cpp
+++ pkg_src/le-gateway/main.cpp
@@ -5,8 +5,17 @@
 #include <cstdio>
 
 #include <plog/Log.h>
+#include <plog/Init.h>
 #include <plog/Appenders/ColorConsoleAppender.h>
+#include <plog/Appenders/RollingFileAppender.h>
+#include <plog/Formatters/TxtFormatter.h>
+
+static plog::ColorConsoleAppender<plog::TxtFormatter> console_appender;
+static plog::RollingFileAppender<plog::TxtFormatter> file_appender("/var/log/le-gateway.log", 2*1024*1024, 5);
 
 int main(void) {
+  plog::init(plog::verbose, &file_appender).addAppender(&console_appender);
+
+
   return 0;
 }
