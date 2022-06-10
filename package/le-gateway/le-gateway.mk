f20c59eee1eb7ff04fcf72cc0b174d6add55e558
Kim Tae-Hee <thkim7816@gmail.com>
2022-06-10 22:09:47 +0900

    수정: buildroot package에 포함된 nlohmannjson으로 변경

--- package/le-gateway/le-gateway.mk
+++ package/le-gateway/le-gateway.mk
@@ -8,7 +8,7 @@ LE_GATEWAY_SITE:=$(TOPDIR)/../pkg_src/le-gateway
 LE_GATEWAY_SITE_METHOD:=local
 LE_GATEWAY_INSTALL_STAGING:=NO
 LE_GATEWAY_INSTALL_TARGET:=YES
-LE_GATEWAY_DEPENDENCIES := plog nlohmannjson sdbuscpp
+LE_GATEWAY_DEPENDENCIES := plog json-for-modern-cpp sdbuscpp
 
 
 $(eval $(cmake-package))
